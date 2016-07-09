a5bfa116697c7598da4c3dbea52261919d8bd0a6
kenneth.kasajian <kenneth.kasajian@schneider-electric.com>
2016-07-08 21:02:29 -0700

    IList

--- SharedMemory/Array.cs
+++ SharedMemory/Array.cs
@@ -26,11 +26,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
-using System.Text;
-using System.Threading;
 
 namespace SharedMemory
 {
