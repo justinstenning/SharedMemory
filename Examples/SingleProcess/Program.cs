f1d8fa6d7f178c41b647687654f4d162a0f906e5
Justin Stenning <spazzarama@gmail.com>
2016-07-24 20:32:13 +1000

    Added command line args to SingleProcess example
    This allows benchmarking different combinations of readers/writes buffer sizes and so on.

--- Examples/SingleProcess/Program.cs
+++ Examples/SingleProcess/Program.cs
@@ -31,6 +31,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
+using CommandLine;
 #if NET40Plus
 using System.Threading.Tasks;
 #endif
@@ -39,16 +40,66 @@ namespace SingleProcess
 {
     class Program
     {
+        class AppArguments
+        {
+            [Argument(ArgumentType.AtMostOnce, ShortName = "b", DefaultValue = 1048576, HelpText = "The buffer size.")]
+            public int bufferSize = 0;
+            [Argument(ArgumentType.AtMostOnce, ShortName = "n", DefaultValue = 50, HelpText = "The number of nodes.")]
+            public int nodeCount = 0;
+            [Argument(ArgumentType.Required, ShortName = "w", HelpText = "The number of writers.")]
+            public int writers = 0;
+            [Argument(ArgumentType.Required, ShortName = "r", HelpText = "The number of readers.")]
+            public int readers = 0;
+            [Argument(ArgumentType.AtMostOnce, ShortName = "e", DefaultValue = 100000, HelpText = "The number of elements to process.")]
+            public int elements = 0;
+        }
+
         static void Main(string[] args)
         {
+            int elements = 100000;
             int writeCount = 0;
             int clientWaitCount = 0;
             int readCount = 0;
             int serverWaitCount = 0;
             long lastTick = 0;
-            byte[] readData = new byte[1048576];
-            int size = sizeof(byte) * 1048576;
-            int count = 2; // node count within buffer
+            int bufferSize = 1048576;
+            int size = sizeof(byte) * bufferSize;
+            int count = 50; // node count within buffer
+
+            int serverCount = 0;
+            int clientCount = 0;
+
+            // Process command line
+            AppArguments parsedArgs = new AppArguments();
+            var validArgs = Parser.ParseArgumentsWithUsage(args, parsedArgs);
+
+            if (!validArgs)
+            {
+                return;
+            }
+            else
+            {
+                elements = parsedArgs.elements;
+                bufferSize = parsedArgs.bufferSize;
+                serverCount = parsedArgs.writers;
+                clientCount = parsedArgs.readers;
+                size = sizeof(byte) * bufferSize;
+                count = parsedArgs.nodeCount;
+            }
+
+            
+            Console.WriteLine("Node buffer size: {0}, count: {1}, writers: {2}, readers {3}, elements: {4}", size, count, serverCount, clientCount, elements);
+
+            int dataListCount = 256;
+            // Generate random data to be written
+            Random random = new Random();
+            byte[][] dataList = new byte[dataListCount][];
+            for (var j = 0; j < dataListCount; j++)
+            {
+                var data = new byte[size];
+                random.NextBytes(data);
+                dataList[j] = data;
+            }
 
             long bytesWritten = 0;
             long bytesRead = 0;
@@ -79,67 +130,71 @@ namespace SingleProcess
                         Interlocked.Increment(ref readCount);
                     stopTick = clientTime.ElapsedTicks;
 
-                    if (writeCount > 100000 && writeCount - readCount == 0)
+                    if (writeCount > elements && writeCount - readCount == 0)
                         break;
                 }
             };
+            for (int c = 0; c < clientCount; c++)
+            {
 #if NET40Plus
-            Task c1 = Task.Factory.StartNew(clientAction);
-            Task c2 = Task.Factory.StartNew(clientAction);
-            Task c3 = Task.Factory.StartNew(clientAction);
-            Task c4 = Task.Factory.StartNew(clientAction);
-            //Task c5 = Task.Factory.StartNew(clientAction);
-            //Task c6 = Task.Factory.StartNew(clientAction);
-            //Task c7 = Task.Factory.StartNew(clientAction);
+                Task c1 = Task.Factory.StartNew(clientAction);
 #else
-            ThreadPool.QueueUserWorkItem((o) => { clientAction(); });
-            ThreadPool.QueueUserWorkItem((o) => { clientAction(); });
-            ThreadPool.QueueUserWorkItem((o) => { clientAction(); });
-            ThreadPool.QueueUserWorkItem((o) => { clientAction(); });
+                ThreadPool.QueueUserWorkItem((o) => { clientAction(); });
 #endif
+            }
+            bool wait = true;
             int index = 0;
-
             Action serverWrite = () =>
             {
                 int serverIndex = Interlocked.Increment(ref index);
 
                 var writer = (serverIndex == 1 ? server : new SharedMemory.CircularBuffer(name));
-
+                bool done = false;
+                TimeSpan doneTime = TimeSpan.MinValue;
                 for (; ; )
                 {
-                    if (writeCount <= 100000)
+                    if (writeCount <= elements)
                     {
-                        int amount = writer.Write(readData, 100);
+                        int amount = writer.Write(dataList[random.Next(0, dataListCount)], 100);
                         bytesWritten += amount;
                         if (amount == 0)
                             Interlocked.Increment(ref serverWaitCount);
                         else
                             Interlocked.Increment(ref writeCount);
                     }
+                    else
+                    {
+                        if (!done && serverIndex == 1)
+                        {
+                            doneTime = sw.Elapsed;
+                            done = true;
+                        }
+                    }
 
                     if (serverIndex == 1 && sw.ElapsedTicks - lastTick > 1000000)
                     {
-                        lastTick = sw.ElapsedTicks;
                         Console.WriteLine("Write: {0}, Read: {1}, Diff: {5}, Wait(cli/svr): {3}/{2}, {4}MB/s", writeCount, readCount, serverWaitCount, clientWaitCount, (int)((((bytesWritten + bytesRead) / 1048576.0) / sw.ElapsedMilliseconds) * 1000), writeCount - readCount);
-
-                        if (writeCount > 100000 && writeCount - readCount == 0)
+                        lastTick = sw.ElapsedTicks;
+                        if (writeCount > elements && writeCount - readCount == 0)
+                        {
+                            Console.WriteLine("Total Time: " + doneTime);
+                            wait = false;
                             break;
+                        }
                     }
                 }
             };
 
+            for (int s = 0; s < serverCount; s++)
+            {
 #if NET40Plus
-            Task s1 = Task.Factory.StartNew(serverWrite);
-            //Task s2 = Task.Factory.StartNew(serverWrite);
-            //Task s3 = Task.Factory.StartNew(serverWrite);
-            //Task s4 = Task.Factory.StartNew(serverWrite);
-            //Task s5 = Task.Factory.StartNew(serverWrite);
-            //Task s6 = Task.Factory.StartNew(serverWrite);
-            //Task s7 = Task.Factory.StartNew(serverWrite);
+                Task s1 = Task.Factory.StartNew(serverWrite);
 #else
-            ThreadPool.QueueUserWorkItem((o) => { serverWrite(); });
+                ThreadPool.QueueUserWorkItem((o) => { serverWrite(); });
 #endif
-            Console.ReadLine();
+            }
+            while (wait)
+                Thread.Sleep(100);
         }
     }
 }
